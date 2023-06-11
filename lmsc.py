1593ed6f55167441058c976c31ab4018a0f6245c
Monish Reddy Kotturu <monishkotturu@gmail.com>
2023-06-11 13:56:56 -0400

    laie proto

--- lmsc.py
+++ lmsc.py
@@ -1,30 +1,28 @@
 from time import ctime, time
 
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.patches import ConnectionPatch
 import numpy as np
 from numba import njit, prange
 from scipy.sparse.csgraph import laplacian
+from scipy import stats
+from scipy.linalg import svdvals
 import networkx as nx
 
 from graph_optimization import fdla_weights_symmetric, fmmc_weights, lmsc_weights, fastest_averaging_constant_weight, max_degree_weights, metropolis_hastings_weights
 
 
 def main():
-	N = 10
-	runs = 1000
-	T = 1000
+	num_arms = 4
+	num_runs = 100
+	num_timesteps = 3000
+	num_agents = 50
 
 	# adjacency matrices
 	As = [
-		# # all-to-all
-		# np.array([
-		# 	[0, 1, 1, 1, 1],
-		# 	[1, 0, 1, 1, 1],
-		# 	[1, 1, 0, 1, 1],
-		# 	[1, 1, 1, 0, 1],
-		# 	[1, 1, 1, 1, 0],
-		# ]),
+		# all-to-all auto
+		nx.to_numpy_array(nx.complete_graph(num_agents)),
 		# # house
 		# np.array([
 		# 	[0, 1, 1, 0, 0],
@@ -33,14 +31,8 @@ def main():
 		# 	[0, 1, 0, 0, 1],
 		# 	[0, 0, 1, 1, 0],
 		# ]),
-		# # ring
-		# np.array([
-		# 	[0, 1, 0, 0, 1],
-		# 	[1, 0, 1, 0, 0],
-		# 	[0, 1, 0, 1, 0],
-		# 	[0, 0, 1, 0, 1],
-		# 	[1, 0, 0, 1, 0],
-		# ]),
+		# ring
+		nx.to_numpy_array(nx.cycle_graph(num_agents)),
 		# # line
 		# np.array([
 		# 	[0, 1, 0, 0, 0],
@@ -49,14 +41,14 @@ def main():
 		# 	[0, 0, 1, 0, 1],
 		# 	[0, 0, 0, 1, 0],
 		# ]),
-		# star
-		np.array([
-			[0, 1, 1, 1, 1],
-			[1, 0, 0, 0, 0],
-			[1, 0, 0, 0, 0],
-			[1, 0, 0, 0, 0],
-			[1, 0, 0, 0, 0],
-		]),
+		# # star
+		# np.array([
+		# 	[0, 1, 1, 1, 1],
+		# 	[1, 0, 0, 0, 0],
+		# 	[1, 0, 0, 0, 0],
+		# 	[1, 0, 0, 0, 0],
+		# 	[1, 0, 0, 0, 0],
+		# ]),
 		# # 8-agents
 		# np.array([
 		# 	[0, 1, 1, 1, 0, 0, 0, 0],
@@ -68,18 +60,14 @@ def main():
 		# 	[0, 0, 0, 1, 1, 1, 0, 1],
 		# 	[0, 0, 0, 1, 1, 1, 1, 0],
 		# ]),
+		# # large 50
+		# np.load('data/saved_networks/large_50_adj.npy'),
 	]
 
 	# corresponding incidence matrices
 	Is = [
-		# # all-to-all
-		# np.array([
-		# 	[ 1,  1,  1,  1,  0,  0,  0,  0,  0,  0],
-		# 	[-1,  0,  0,  0,  1,  1,  1,  0,  0,  0],
-		# 	[ 0, -1,  0,  0, -1,  0,  0,  1,  1,  0],
-		# 	[ 0,  0, -1,  0,  0, -1,  0, -1,  0,  1],
-		# 	[ 0,  0,  0, -1,  0,  0, -1,  0, -1, -1],
-		# ]),
+		# all-to-all auto
+		np.asarray(nx.linalg.graphmatrix.incidence_matrix(nx.complete_graph(num_agents), oriented=True).todense()),
 		# # house
 		# np.array([
 		# 	[ 1,  0,  0,  0, -1,  0],
@@ -88,14 +76,8 @@ def main():
 		# 	[ 0,  0, -1,  1,  0,  0],
 		# 	[ 0, -1,  1,  0,  0,  0],
 		# ]),
-		# # ring
-		# np.array([
-		# 	[ 1,  0,  0,  0, -1],
-		# 	[-1,  1,  0,  0,  0],
-		# 	[ 0, -1,  1,  0,  0],
-		# 	[ 0,  0, -1,  1,  0],
-		# 	[ 0,  0,  0, -1,  1],
-		# ]),
+		# ring
+		np.asarray(nx.linalg.graphmatrix.incidence_matrix(nx.cycle_graph(num_agents), oriented=True).todense()),
 		# # line
 		# np.array([
 		# 	[ 1,  0,  0,  0],
@@ -104,14 +86,14 @@ def main():
 		# 	[ 0,  0, -1,  1],
 		# 	[ 0,  0,  0, -1],
 		# ]),
-		# star
-		np.array([
-			[ 1,  1,  1,  1],
-			[-1,  0,  0,  0],
-			[ 0, -1,  0,  0],
-			[ 0,  0, -1,  0],
-			[ 0,  0,  0, -1],
-		]),
+		# # star
+		# np.array([
+		# 	[ 1,  1,  1,  1],
+		# 	[-1,  0,  0,  0],
+		# 	[ 0, -1,  0,  0],
+		# 	[ 0,  0, -1,  0],
+		# 	[ 0,  0,  0, -1],
+		# ]),
 		# # 8-agents
 		# np.array([
 		# 	[  1,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
@@ -123,18 +105,24 @@ def main():
 		# 	[  0,  0,  0,  0,  0,  0,  0,  0,  0, -1,  0,  0, -1,  0, -1,  0,  1],
 		# 	[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1,  0,  0, -1,  0, -1, -1],
 		# ]),
+		# # large 50
+		# np.load('data/saved_networks/large_50_inc.npy'),
 	]
 
 	networks = [
-		# 'all-to-all',
+		'all-to-all',
 		# 'house',
-		# 'ring',
+		'ring',
 		# 'line',
-		'star',
+		# 'star',
 		# '8-agents',
+		# 'large_50',
 	]
 
-	trueMeans = np.array([np.random.normal(0, 1, N) for _ in range(runs)])
+	# trueMeans = np.array([np.random.normal(0, 1, num_arms) for _ in range(num_runs)])
+	trueMeans = np.array([[0.2646,  0.6135, 0.8950, 0.5764] for _ in range(num_runs)])
+	trueMeansGroups = np.array([[[0.0149,  0.7161, 0.7944, 0.6749], [0.5144,  0.5108, 0.9955, 0.4778]] for _ in range(num_runs)])
+	# trueMeans = np.sort(trueMeans, axis=1)[:, ::-1]
 
 	# sizing
 	SMALL_SIZE = 10
@@ -142,6 +130,7 @@ def main():
 	LARGE_SIZE = 18
 
 	# plt.rcParams["figure.figsize"] = (15, 8)
+	matplotlib.use('Agg')
 	plt.rc('font', size=SMALL_SIZE)
 	plt.rc('axes', titlesize=MEDIUM_SIZE, labelsize=MEDIUM_SIZE)
 	plt.rc('xtick', labelsize=MEDIUM_SIZE)
@@ -181,13 +170,13 @@ def main():
 		print(f'Network: {networks[mat_idx]}')
 		Ps, rhos, labels = [], [], []
 
-		for k in [0.02]:
-			P, rho = generateP(As[mat_idx], kappa=k)
-			Ps.append(P)
-			rhos.append(rho)
-			print(f'{"kappa " + str(k):<20s}: {rho} {(1 / np.log(1 / rho))}')
-			labels.append(fr'$\kappa$ = {k}')
-			# print(P)
+		# for k in [0.02]:
+		# 	P, rho = generateP(As[mat_idx], kappa=k)
+		# 	Ps.append(P)
+		# 	rhos.append(rho)
+		# 	print(f'{"kappa " + str(k):<20s}: {rho} {(1 / np.log(1 / rho))}')
+		# 	labels.append(fr'$\kappa$ = {k}')
+		# 	print(P)
 
 		# constant edge
 		alpha, _, P, rho = fastest_averaging_constant_weight(Is[mat_idx])
@@ -216,13 +205,13 @@ def main():
 		labels.append(fr'Local-degree')
 		# print(P)
 
-		# fmmc
-		_, P, rho = fmmc_weights(Is[mat_idx])
-		rho = get_rho(P)
-		Ps.append(P)
-		rhos.append(rho)
-		print(f'{"FMMC":<20s}: {rho} {(1 / np.log(1 / rho))}')
-		labels.append('FMMC')
+		# # fmmc
+		# _, P, rho = fmmc_weights(Is[mat_idx])
+		# rho = get_rho(P)
+		# Ps.append(P)
+		# rhos.append(rho)
+		# print(f'{"FMMC":<20s}: {rho} {(1 / np.log(1 / rho))}')
+		# labels.append('FMMC')
 		# print(P)
 
 		# fdla
@@ -232,25 +221,42 @@ def main():
 		rhos.append(rho)
 		print(f'{"FDLA":<20s}: {rho} {(1 / np.log(1 / rho))}')
 		labels.append('FDLA')
+		# print(P)
 
-		# lmsc
-		_, P = lmsc_weights(Is[mat_idx])
+		# # lmsc
+		# _, P = lmsc_weights(Is[mat_idx])
+		# rho = get_rho(P)
+		# Ps.append(P)
+		# rhos.append(rho)
+		# print(f'{"LMSC":<20s}: {rho} {(1 / np.log(1 / rho))}')
+		# labels.append('LMSC')
+		# print(P)
+
+		# average weights
+		P = generateAvgW(As[mat_idx])
 		rho = get_rho(P)
 		Ps.append(P)
 		rhos.append(rho)
-		print(f'{"LMSC":<20s}: {rho} {(1 / np.log(1 / rho))}')
-		labels.append('LMSC')
-		print(P)
+		print(f'{"Avg":<20s}: {rho} {(1 / np.log(1 / rho))}')
+		labels.append('Avg')
+		print(spectralGap(P))
 		print('\n\n')
 
 		fig, ax = plt.subplots()
 
 		for idx, P in enumerate(Ps):
-			reg = run(runs, N, T, trueMeans, P)
-			reg = np.mean(reg, axis=0)	# mean over runs
+			# coopucb2
+			# reg = coopucb2(num_runs, num_arms, num_timesteps, trueMeans, P)
 
+			# d-UER
+			reg = dUER(num_runs, num_arms, num_timesteps, trueMeansGroups, P)
+
+			# save regret
+			reg = np.mean(reg, axis=0)	# mean over runs
 			np.save(f'data/data/new_{networks[mat_idx].replace(" ", "-")}_reg_{labels[idx]}.npy', reg)
-			# reg = np.load(f'testdata/new_{networks[mat_idx].replace(" ", "-")}_reg_{labels[idx]}.npy')
+
+			# comment previous lines and uncomment the following line to read regret values from file
+			# reg = np.load(f'data/data/new_{networks[mat_idx].replace(" ", "-")}_reg_{labels[idx]}.npy')
 
 			# plot regret
 			fig.suptitle(f'{networks[mat_idx].title()} network')
@@ -267,10 +273,76 @@ def main():
 		fig.suptitle(f'{networks[mat_idx].title()} network')
 		fig.supxlabel('Timesteps')
 		fig.supylabel('Mean estimate error for the best arm')
+		# plt.show()
 
 
 @njit(parallel=True)
-def run(runs: int, N: int, T: int, trueMeans: np.ndarray, P: np.ndarray, noisy_agents=np.array([1])) -> tuple:
+def laie(runs: int, num_arms: int, num_timesteps: int, trueMeans: np.ndarray, P: np.ndarray, noisy_agents=np.array([1])) -> tuple:
+	'''
+	Plays LAIE given the number of runs, number of arms, timesteps, true
+	means of arms, and the P matrix of the network. Optimized to work with
+	numba.
+	'''
+
+	num_agents, _ = P.shape
+	regret = np.zeros((runs, num_agents, num_timesteps))
+
+	for run in prange(runs):
+		# one experiment of LAIE
+		pass
+
+	return regret
+
+
+@njit(parallel=True)
+def dUER(runs: int, num_arms: int, num_timesteps: int, trueMeans: np.ndarray, P: np.ndarray, noisy_agents=np.array([1])) -> tuple:
+	'''
+	Plays d-UER given the number of runs, number of arms, timesteps, true
+	means of arms, and the P matrix of the network. Optimized to work with
+	numba.
+	'''
+
+	num_agents, _ = P.shape
+	regret = np.zeros((runs, num_agents, num_timesteps))
+	d = 2
+
+	for run in prange(runs):
+		# initialize
+		# each action is played once
+		estimates = np.zeros((num_agents, num_arms)) # phi
+		private_observations = np.zeros((num_agents, num_arms))
+		for i in range(num_agents):
+			for k in range(num_arms):
+				# reward
+				# "int(i / num_agents >= 0.5)" puts the first half of the agents in group 0 and the rest in group 1
+				private_observations[i, k] = np.random.normal(trueMeans[run, int(i / num_agents >= 0.5), k], 1)
+		num_pulls = np.ones(num_arms)
+		I = np.zeros(num_agents, dtype=np.int64)
+
+		for t in range(num_timesteps):
+			for i in range(num_agents):
+				temp_sum = np.zeros((num_agents, num_arms))
+				for j in range(num_agents):
+					temp_sum[j] = P[i, j] * estimates[j]
+				estimates[i] = np.sum(temp_sum, axis=0) + private_observations[i]
+				C = np.sqrt(2 * np.log(t) * ((1 / (num_agents * num_pulls)) + (2 * d / np.power(num_pulls, 2))))
+				I[i] = np.argmax((estimates[i] / num_pulls) + C)
+
+			# most_freq_arm = stats.mode(I)[0] # not compatible with numba
+			# most_freq_arm = I[np.argmax(np.unique(I, return_counts=True)[1])] # not compatible with numba
+			most_freq_arm = np.argmax(np.bincount(I)) # compatible with numba
+			num_pulls[most_freq_arm] += 1
+			private_observations = np.zeros((num_agents, num_arms))
+			for i in range(num_agents):
+				# reward
+				private_observations[i, most_freq_arm] = np.random.normal(trueMeans[run, int(i / num_agents >= 0.5), most_freq_arm], 1)
+				regret[run, i, t] = np.abs(trueMeans[run, int(i / num_agents >= 0.5), most_freq_arm] - np.max(trueMeans[run, int(i / num_agents >= 0.5), :]))
+
+	return regret
+
+
+@njit(parallel=True)
+def coopucb2(runs: int, num_arms: int, num_timesteps: int, trueMeans: np.ndarray, P: np.ndarray, noisy_agents=np.array([1])) -> tuple:
 	'''
 	Plays coopucb2 given the number of runs, number of arms, timesteps, true
 	means of arms, and the P matrix of the network. Optimized to work with
@@ -282,59 +354,72 @@ def run(runs: int, N: int, T: int, trueMeans: np.ndarray, P: np.ndarray, noisy_a
 	f = lambda t : np.sqrt(np.log(t))
 	Geta = 1 - (eta ** 2)/16
 	var = 1		# variance for the gaussian distribution behind each arm
-	M, _ = P.shape
+	num_agents, _ = P.shape
 	x1 = 2 * gamma / Geta
 
-	reg = np.zeros((runs, M, T))
+	regret = np.zeros((runs, num_agents, num_timesteps))
 
 	# run coop-ucb2 "runs" number of times
 	for run in prange(runs):
-		Q = np.zeros((M, N))	# estimated reward
-		n = np.zeros((M, N))	# number of times an arm has been selected by each agent
-		s = np.zeros((M, N))	# cumulative expected reward
-		xsi = np.zeros((M, N))	# number of times that arm has been selected in that timestep
-		rew = np.zeros((M, N))	# reward
-		bestArm = np.max(trueMeans[run])
+		Q = np.zeros((num_agents, num_arms))	# estimated reward
+		n = np.zeros((num_agents, num_arms))	# number of times an arm has been selected by each agent
+		s = np.zeros((num_agents, num_arms))	# cumulative expected reward
+		xsi = np.zeros((num_agents, num_arms))	# number of times that arm has been selected in that timestep
+		reward = np.zeros((num_agents, num_arms))	# reward
+		bestArmMean = np.max(trueMeans[run])
 		bestArmIdx = np.argmax(trueMeans[run])
 
-		for t in range(T):
+		for t in range(num_timesteps):
 			_t = t - 1 if t > 0 else 0
-			if t < N:
-				for k in range(M):
-					rew[k] = np.zeros(N)
-					xsi[k] = np.zeros(N)
+			if t < num_arms:
+				for k in range(num_agents):
+					reward[k] = np.zeros(num_arms)
+					xsi[k] = np.zeros(num_arms)
 					action = t
 
-					rew[k, action] = np.random.normal(trueMeans[run, action], var)
-					reg[run, k, t] = bestArm - trueMeans[run, action]
+					reward[k, action] = np.random.normal(trueMeans[run, action], var)
+					regret[run, k, t] = bestArmMean - trueMeans[run, action]
 					xsi[k, action] += 1
 			else:
-				for k in range(M):
-					for i in range(N):
+				for k in range(num_agents):
+					for i in range(num_arms):
 						x0 = s[k, i] / n[k, i]
-						x2 = (n[k, i] + f(_t)) / (M * n[k, i])
+						x2 = (n[k, i] + f(_t)) / (num_agents * n[k, i])
 						x3 = np.log(_t) / n[k, i]
-						_explr = sigma_g * np.sqrt(x1 * x2 * x3)
-						Q[k, i] = x0 + _explr
+						C = sigma_g * np.sqrt(x1 * x2 * x3)
+						Q[k, i] = x0 + C
 
-					rew[k] = np.zeros(N)
-					xsi[k] = np.zeros(N)
+					reward[k] = np.zeros(num_arms)
+					xsi[k] = np.zeros(num_arms)
 
 					action = np.argmax(Q[k, :])
-					rew[k, action] = np.random.normal(trueMeans[run, action], var)
-					reg[run, k, t] = bestArm - trueMeans[run, action]
+					reward[k, action] = np.random.normal(trueMeans[run, action], var)
+					regret[run, k, t] = bestArmMean - trueMeans[run, action]
 					xsi[k, action] += 1
 
-			for k in noisy_agents:
-				# add noise
-				rew[k, :] *= -1 * np.abs(np.random.normal(0, 1))
+			# # add noise
+			# for k in noisy_agents:
+			# 	rew[k, :] *= -1 * np.abs(np.random.normal(0, 1))
 
 			# update estimates using running consensus
-			for i in range(N):
+			for i in range(num_arms):
 				n[:, i] = P @ (n[:, i] + xsi[:, i])
-				s[:, i] = P @ (s[:, i] + rew[:, i])
+				s[:, i] = P @ (s[:, i] + reward[:, i])
 
-	return reg
+	return regret
+
+
+def spectralGap(P):
+	r'''
+	spectral gap of P = 1 - second largest SINGULAR VALUE of P
+	'''
+	singular_values = np.unique(np.linalg.svd(P, compute_uv=False))
+	singular_values.sort()
+	print(singular_values)
+	return 1 - singular_values[-2]
+
+def generateAvgW(A):
+	return (1 / np.sum(A, axis=0)) * A
 
 def generateP(A, kappa):
 	dmax = np.max(np.sum(A, axis=0))
